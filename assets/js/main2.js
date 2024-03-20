20592cec4464fc51a215013635e7a52294b16a7f
haakel <haakel213@gmail.com>
2024-03-20 15:23:18 +0330

    s 06 - e14

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -1,5 +1,38 @@
 jQuery(document).ready(function () {
+    // قسمت11
+
     // jQuery('span').parent().css("border-color","red");
     // jQuery('span').parents().css("border-color","red");
-    jQuery('span').parentsUntil("div").css("border-color","red");
+    // jQuery('span').parentsUntil("div").css("border-color","red");
+
+
+    // قسمت 12 
+
+    // jQuery('div').children().css("border-color","blue");
+    // jQuery('ul').children().css("border-color","blue");
+    // jQuery('li').children().css("border-color","blue");
+    // jQuery('div').find("span").css("border-color","blue");
+    // jQuery('h3').siblings("p").css("border-color","blue");
+    // jQuery('p').next().css("border-color","blue");
+    // jQuery('h2').nextAll().css("border-color","blue");
+    // jQuery('h2').nextUntil("h5").css("border-color","blue");
+    // jQuery('h2').prev().css("border-color","blue");
+    // jQuery('h2').prevAll().css("border-color","blue");
+    // jQuery('h6').prevUntil("span").css("border-color","blue");
+    
+
+    //قسمت 13
+    //اولین تگی که باشه رو انتخاب میکنه 
+    // jQuery('p').first().css("border-color","#28ff2c");
+    //اخرین تگی که باشه رو انتخاب میکنه 
+    // jQuery('p').last().css("border-color","#28ff2c");
+    //انختاب میکنی چندمین تگ رو انتخاب کنه 
+    // jQuery('p').eq(3).css("border-color","#28ff2c");
+    // میره ونی که براش فیلترش رو مشخص کردی رو انتخاب میکنه ، مثلا اونی که کلاس تست داره
+    // jQuery('p').filter(".test").css("border-color","#28ff2c");
+    //میره به جز اونایی که مثلا کساس تکس رو داره رو انتخاب کنه 
+    // jQuery('p').not(".test").css("border-color","#28ff2c");
+    
+    // قسمت 14 
+    
 });
---
+++ assets/js/main3.js
@@ -0,0 +1,15 @@
+let j = jQuery.noConflict();
+j(document).ready(function () {
+    let tabs = j(".tabs li a");
+    tabs.click(function () { 
+        let content = this.hash.replace('/','');
+        tabs.removeClass("active");
+        j(this).addClass("active");
+        j("#content").find("div").hide();
+        j(content).fadeIn(200);
+        // j(content).show(200);
+
+    });
+    
+
+});
