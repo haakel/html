2e29ff62b53df4404432d49a8cc095c15c05c244
haakel <haakel213@gmail.com>
2024-04-27 17:53:43 +0330

    s07-e03

--- assets/js/main6.js
+++ assets/js/main6.js
@@ -1,16 +1,51 @@
-let j = jQuery.noConflict;
+let j = jQuery.noConflict();
 
 j(document).ready(function () {
    j('#login-form').on('submit',function(e){
     e.preventDefault();
+    let el = j(this);
+    let loader =j(".loading");
+    let btn_submit =j("#submit");
     // alert("test");
     let username = j("#username").val();
     let passsword = j("#passsword").val();
     // console.log(username);
 
-    // try this here
-    let csrf = j("#login-form").data("csrf");
-    console.log(csrf);
+    // let csrf = j("#login-form").data("csrf");
+    let csrf = el.data("csrf");
+    // console.log(csrf);
+    j.ajax({
+        url:"login.php",
+        type:"POST",
+        // dataType:JSON,
+        data:{
+            action:"userlogin",
+            username : username,
+            password : passsword,
+            csrf : csrf
+        },
+        beforeSend : function(){
+            loader.show();
+            btn_submit.text("در حال ارسال ....");
+        }, 
+        success : function(response){
+            alert("ورود شما موفقیت آمیز بوده است!");
+        }, 
+        error : function(error){
+            alert("خطایی در ورود شما رخ داده است !");
+        },
+        complete : function(){
+            loader.hide();
+            btn_submit.text("ورود");
 
+        },
+
+
+    });
    })
 });
+
+ 
+
+
+  
---
+++ login.php
@@ -0,0 +1,6 @@
+<?php
+$username = $_POST["username"];
+// var_dump($_POST);
+echo"$username";
+
+?>
