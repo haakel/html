ab1960b291ecb6ba668d25a50b787ce6c3697357
haakel <haakel213@gmail.com>
2024-06-02 17:29:58 +0330

    s7 - e4

--- assets/js/main6.js
+++ assets/js/main6.js
@@ -17,9 +17,9 @@ j(document).ready(function () {
     j.ajax({
         url:"login.php",
         type:"POST",
-        // dataType:JSON,
+        // dataType:JSON,   
         data:{
-            action:"userlogin",
+            action:"do_register",
             username : username,
             password : passsword,
             csrf : csrf
@@ -29,7 +29,47 @@ j(document).ready(function () {
             btn_submit.text("در حال ارسال ....");
         }, 
         success : function(response){
-            alert("ورود شما موفقیت آمیز بوده است!");
+            // alert("ورود شما موفقیت آمیز بوده است!");
+
+            let jsonData = JSON.parse(response);
+            // alert(jsonData);
+            if(jsonData.success == true){
+            // alert(jsonData.message);
+            j.toast({
+                text: jsonData.message, // Text that is to be shown in the toast
+                heading: 'Note', // Optional heading to be shown on the toast
+                icon: 'success', // Type of toast icon
+                showHideTransition: 'slide', // fade, slide or plain
+                allowToastClose: true, // Boolean value true or false
+                hideAfter: 3000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+                stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+                position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+                
+                
+                
+                textAlign: 'left',  // Text alignment i.e. left, right or center
+                loader: true,  // Whether to show loader or not. True by default
+                loaderBg: '#ffffff',  // Background color of the toast loader
+            });
+            }else{
+                j.toast({
+                    text: jsonData.message, // Text that is to be shown in the toast
+                    heading: 'Note', // Optional heading to be shown on the toast
+                    icon: 'error', // Type of toast icon
+                    showHideTransition: 'slide', // fade, slide or plain
+                    allowToastClose: true, // Boolean value true or false
+                    hideAfter: 3000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+                    stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+                    position: 'top-center', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+                    
+                    
+                    
+                    textAlign: 'left',  // Text alignment i.e. left, right or center
+                    loader: true,  // Whether to show loader or not. True by default
+                    loaderBg: '#ffffff',  // Background color of the toast loader
+                });
+            }
+
         }, 
         error : function(error){
             alert("خطایی در ورود شما رخ داده است !");
--- login.php
+++ login.php
@@ -1,6 +1,77 @@
 <?php
-$username = $_POST["username"];
+// $username = $_POST["username"];
 // var_dump($_POST);
-echo"$username";
+// echo"$username";
+
+
+ // قسمت 3 فصل 7
+// if(!empty($_POST["username"])){ 
+//   echo  json_encode([
+//         "success" => true,
+//         "message"=>"login",
+//         "status" => 200
+//     ]);
+// }else{
+//   echo  json_encode([
+//         "success" => false ,
+//         "message"=>"مشکلی در ورود پیش آمده",
+//         "status" => 404
+//     ]);
+// }
+if (isset($_POST["action"])&& !empty($_POST["action"]) && function_exists($_POST["action"])){
+    switch ($_POST["action"]){
+        case "do_login":
+            do_login();
+                break;
+            case "do_register":
+                do_register ();
+                break;
+    }
+
+
+    
+}else{
+    echo  json_encode([
+        "success" => false ,
+        "message"=>"خطایی رخ داده است ",
+        "status" => 404
+    ]);
+}
+ // قسمت 4 فصل 7
+function do_login() {
+    //login
+
+    if(!empty($_POST["username"])){ 
+  echo  json_encode([
+        "success" => true,
+        "message"=>"ورود موفقیت آمیز بوده است",
+        "status" => 200
+    ]);
+}else{
+  echo  json_encode([
+        "success" => false ,
+        "message"=>"مشکلی در ورود پیش آمده",
+        "status" => 404
+    ]);
+}
+};
+function do_register() {
+    //login
+
+    if(!empty($_POST["username"])){ 
+  echo  json_encode([
+        "success" => true,
+        "message"=>"ثبت نام موفقیت آمیز بوده است ",
+        "status" => 200
+    ]);
+}else{
+  echo  json_encode([
+        "success" => false ,
+        "message"=>"مشکلی در ثبت نام پیش آمده",
+        "status" => 404
+    ]);
+}
+};
+
 
 ?>
